51f2716dda271b1c8d2da3ae6241e14240d1c4c8
Lara Martin <larikimc@gmail.com>
2017-03-10 21:10:23 +0100

    load cursor in DetailActivity and print historical data for testing

--- app/src/main/java/eu/laramartin/stockhawk/ui/DetailsActivity.java
+++ app/src/main/java/eu/laramartin/stockhawk/ui/DetailsActivity.java
@@ -1,13 +1,24 @@
 package eu.laramartin.stockhawk.ui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import eu.laramartin.stockhawk.R;
+import eu.laramartin.stockhawk.data.Contract;
 
-public class DetailsActivity extends AppCompatActivity {
+import static com.github.mikephil.charting.charts.Chart.LOG_TAG;
+
+public class DetailsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int STOCK_LOADER = 0;
+    private String symbol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +26,29 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
         Intent intent = getIntent();
         if (intent.getExtras() != null) {
-            String symbol = intent.getExtras().getString(getString(R.string.intent_stock_symbol_key));
+            symbol = intent.getExtras().getString(getString(R.string.intent_stock_symbol_key));
             Toast.makeText(this, "symbol: " + symbol, Toast.LENGTH_SHORT).show();
         }
+        getSupportLoaderManager().initLoader(STOCK_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this,
+                Contract.Quote.makeUriForStock(symbol),
+                new String[]{Contract.Quote.COLUMN_HISTORY},
+                null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        data.moveToFirst();
+        String history = data.getString(data.getColumnIndex(Contract.Quote.COLUMN_HISTORY));
+        Log.v(LOG_TAG, "historical data: " + history);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
