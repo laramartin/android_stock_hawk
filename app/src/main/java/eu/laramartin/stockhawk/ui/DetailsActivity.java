d0cea74c2bd94e6fde4a056a4b3db440ce3cd1a3
Lara Martin <larikimc@gmail.com>
2017-03-11 21:25:41 +0100

    print chart with reverse order

--- app/src/main/java/eu/laramartin/stockhawk/ui/DetailsActivity.java
+++ app/src/main/java/eu/laramartin/stockhawk/ui/DetailsActivity.java
@@ -7,26 +7,36 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import eu.laramartin.stockhawk.R;
 import eu.laramartin.stockhawk.data.Contract;
-
-import static com.github.mikephil.charting.charts.Chart.LOG_TAG;
 
 public class DetailsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final int STOCK_LOADER = 0;
     private String symbol;
+    @BindView(R.id.chart)
+    LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        ButterKnife.bind(this);
         Intent intent = getIntent();
         if (intent.getExtras() != null) {
             symbol = intent.getExtras().getString(getString(R.string.intent_stock_symbol_key));
@@ -47,11 +57,37 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         data.moveToFirst();
         String history = data.getString(data.getColumnIndex(Contract.Quote.COLUMN_HISTORY));
-        List<String> dailyClosingSharePrice = Arrays.asList(history.split("\n"));
+        List<String> weeklyClosingSharePrice = Arrays.asList(history.split("\n"));
 //        Log.v(LOG_TAG, "historical data: " + history);
-        for (String pair : dailyClosingSharePrice) {
-            Log.v(LOG_TAG, "dailyClosingSharePrice: " + pair);
+        List<Entry> chartEntries = new ArrayList<Entry>();
+//        chartEntries.add(new Entry(1.0f, 1.0f));
+//        chartEntries.add(new Entry(2.0f, 2.0f));
+//        chartEntries.add(new Entry(3.0f, 3.0f));
+//        chartEntries.add(new Entry(4.0f, 4.0f));
+        //long dateOfToday = System.currentTimeMillis();
+        int entry = 0;
+        for (String pair : weeklyClosingSharePrice) {
+//            Log.v(LOG_TAG, "weeklyClosingSharePrice: " + pair);
+            List<String> dataPair = Arrays.asList(pair.split(", "));
+//            Log.v(LOG_TAG, "dataPair: " + dataPair);
+//            long dateOfQuote = Long.parseLong(dataPair.get(0));
+//            long dateRelativeInMillis = dateOfQuote - dateOfToday;
+//            float days = dateRelativeInMillis/(24*60*60*1000);
+            chartEntries.add(new Entry(entry,
+                    Float.parseFloat(dataPair.get(1))));
+            entry++;
         }
+        LineDataSet dataSet = new LineDataSet(chartEntries, "Label");
+        dataSet.setColor(R.color.colorAccent);
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate();
+    }
+
+    private String convertTimeInMillisToDate(String dateInMillis) {
+        Date date = new Date(Long.parseLong(dateInMillis) * 1000);
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        return formatter.format(date);
     }
 
     @Override
