d8f7e280c89be368b6a5df36788f7095390e228c
Lara Martin <larikimc@gmail.com>
2017-03-13 19:44:02 +0100

    add widget with test data

---
+++ app/src/main/java/eu/laramartin/stockhawk/widget/WidgetProvider.java
@@ -0,0 +1,30 @@
+package eu.laramartin.stockhawk.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import eu.laramartin.stockhawk.R;
+import eu.laramartin.stockhawk.ui.MainActivity;
+
+/**
+ * Created by lara on 13.03.17.
+ */
+
+public class WidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(
+                    context.getPackageName(),
+                    R.layout.widget_list_item);
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
---
+++ app/src/main/java/eu/laramartin/stockhawk/widget/WidgetRemoteViewsService.java
@@ -0,0 +1,8 @@
+package eu.laramartin.stockhawk.widget;
+
+/**
+ * Created by lara on 13.03.17.
+ */
+
+public class WidgetRemoteViewsService {
+}
