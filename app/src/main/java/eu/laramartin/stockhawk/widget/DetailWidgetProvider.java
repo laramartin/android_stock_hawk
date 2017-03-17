96ca4f84d00cdb4d284bfe4481a67b1b89cc4cfe
Lara Martin <larikimc@gmail.com>
2017-03-17 22:12:07 +0100

    added base of detail widget without data

---
+++ app/src/main/java/eu/laramartin/stockhawk/widget/DetailWidgetProvider.java
@@ -0,0 +1,8 @@
+package eu.laramartin.stockhawk.widget;
+
+/**
+ * Created by lara on 17.03.17.
+ */
+
+public class DetailWidgetProvider extends WidgetProvider {
+}
---
+++ app/src/main/java/eu/laramartin/stockhawk/widget/DetailWidgetRemoteViewsService.java
@@ -0,0 +1,16 @@
+package eu.laramartin.stockhawk.widget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+/**
+ * Created by lara on 17.03.17.
+ */
+
+public class DetailWidgetRemoteViewsService extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return null;
+    }
+}
